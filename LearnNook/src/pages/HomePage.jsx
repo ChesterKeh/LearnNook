e2d2b91901bcadb658ecb877d5f540976dd04a01
Chester Keh <114325356+ChesterKeh@users.noreply.github.com>
2024-02-05 15:48:48 +0800

    Add dependencies and update styling

---
+++ LearnNook/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/NavBar";
+import About from "./AboutPage";
+
+export default function Homepage() {
+  return (
+    <>
+      <Navbar />
+      <About />
+    </>
+  );
+}
